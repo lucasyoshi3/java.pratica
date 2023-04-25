930b7ec4a87e4091f7c83cd0d1082aa52fccc697
lucasyoshi3 <lucasyoshihirosm@gmail.com>
2023-04-25 02:04:30 -0300

    registroAlunos

---
+++ src/registroAluno/Aluno.java
@@ -0,0 +1,23 @@
+package registroAluno;
+
+class Aluno
+{
+    String pnome;
+    String unome;
+    int    pontos;
+
+    // construtor
+    Aluno () {
+        this("" ,"" ,0);
+    }
+
+    Aluno ( String pnomealuno,
+            String unomealuno,
+            int pontosaluno )
+    {
+        pnome   = pnomealuno;
+        unome  = unomealuno;
+        pontos  = pontosaluno;
+    }
+}
+
